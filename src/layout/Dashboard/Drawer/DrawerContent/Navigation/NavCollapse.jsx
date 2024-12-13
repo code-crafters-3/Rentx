70cbc92e4a98dd8c70f0fba10cca90162086a4a7
alan goncalves <alan92goncalves@gmail.com>
2024-12-12 22:05:19 -0300

    feat: sidebar collapse

---
+++ src/layout/Dashboard/Drawer/DrawerContent/Navigation/NavCollapse.jsx
@@ -0,0 +1,128 @@
+import PropTypes from 'prop-types';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Collapse from '@mui/material/Collapse';
+
+import { DownOutlined, UpOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+
+import NavItem from './NavItem';
+import { useGetMenuMaster } from 'api/menu';
+import { useTheme } from '@emotion/react';
+import { matchPath, useLocation } from 'react-router';
+import { useSettingsStore } from '../../../../../zustand/settings';
+
+import HoverTooltip from '../../../../../components/HoverTooltip';
+import { List } from '@mui/material';
+
+import NavCollapseList from './NavCollapseList';
+
+export default function NavCollapse({ item, level }) {
+  const [open, setOpen] = useState(false);
+
+  const theme = useTheme();
+  const { menuMaster } = useGetMenuMaster();
+
+  const { drawerMode } = useSettingsStore();
+
+  const drawerOpen = drawerMode === 'default';
+
+  const openItem = menuMaster.openedItem;
+
+  const { pathname } = useLocation();
+  const isSelected = !!matchPath({ path: item.url, end: false }, pathname) || openItem === item.id;
+
+  const Icon = item.icon;
+  const itemIcon = item.icon ? <Icon style={{ fontSize: drawerOpen ? '1rem' : '1.25rem' }} /> : false;
+
+  const textColor = 'text.primary';
+  const iconSelectedColor = 'primary.main';
+
+  return (
+    <>
+      <HoverTooltip disabled={drawerOpen} content={<NavCollapseList items={item.subItems} />}>
+        <ListItemButton
+          sx={{
+            zIndex: 1201,
+            pl: drawerOpen ? `${level * 28}px` : 1.5,
+            py: !drawerOpen && level === 1 ? 1.25 : 1,
+            ...(drawerOpen && {
+              '&:hover': {
+                bgcolor: 'primary.lighter'
+              },
+              '&.Mui-selected': {
+                bgcolor: 'primary.lighter',
+                borderRight: `2px solid ${theme.palette.primary.main}`,
+                color: iconSelectedColor,
+                '&:hover': {
+                  color: iconSelectedColor,
+                  bgcolor: 'primary.lighter'
+                }
+              }
+            }),
+            ...(!drawerOpen && {
+              '&:hover': {
+                bgcolor: 'transparent'
+              },
+              '&.Mui-selected': {
+                '&:hover': {
+                  bgcolor: 'transparent'
+                },
+                bgcolor: 'transparent'
+              }
+            })
+          }}
+          onClick={() => {
+            console.log('on click');
+            setOpen((value) => !value);
+          }}
+        >
+          {itemIcon && (
+            <ListItemIcon
+              sx={{
+                minWidth: 28,
+                color: isSelected ? iconSelectedColor : textColor,
+                ...(!drawerOpen && {
+                  borderRadius: 1.5,
+                  width: 36,
+                  height: 36,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  '&:hover': {
+                    bgcolor: 'secondary.lighter'
+                  }
+                }),
+                ...(!drawerOpen &&
+                  isSelected && {
+                    bgcolor: 'primary.lighter',
+                    '&:hover': {
+                      bgcolor: 'primary.lighter'
+                    }
+                  })
+              }}
+            >
+              {itemIcon}
+            </ListItemIcon>
+          )}
+          {drawerOpen && (
+            <>
+              <ListItemText primary={item.title} />
+              {open ? <UpOutlined size={5} /> : <DownOutlined />}
+            </>
+          )}
+        </ListItemButton>
+      </HoverTooltip>
+
+      <Collapse in={drawerOpen && open} timeout="auto" unmountOnExit placement="right-start">
+        <List>
+          {item.subItems.map((subItem) => (
+            <NavItem key={subItem.id} item={subItem} level={level + 1} />
+          ))}
+        </List>
+      </Collapse>
+    </>
+  );
+}
+
+NavCollapse.propTypes = { item: PropTypes.object, level: PropTypes.number };
